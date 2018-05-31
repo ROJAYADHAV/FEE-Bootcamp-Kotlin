64fb5ede31892cc62782c6409006951d8f31695d
chandanajampala <jampala.c@gmail.com>
2018-06-01 00:31:08 +0530

    partial evaluatin done

--- js/keyboardevents.js
+++ js/keyboardevents.js
@@ -1,8 +1,17 @@
+
+var result ="";
+var display="";
+var num="";
+
+
+localStorage.setItem("result",result);
+localStorage.setItem("display",display);
+localStorage.setItem("num",num);
 function getNumber(evt){
    
       
  var charCode = (evt.which) ? evt.which : event.keyCode  //reflects the typed content
-   console.log(charCode);
+  /* console.log(charCode);
    if (charCode > 31 && (charCode < 42 || charCode > 57)  && charCode==44 && charCode !=61)
       return false;
 
@@ -15,7 +24,49 @@ function getNumber(evt){
    }
 */
 
+//document.getElementById("span").innerHTML = String.fromCharCode(charCode);
 
 
+if(charCode == 13){
+    localStorage.display="";
+    localStorage.result = eval(localStorage.getItem("result"));
+    setVal();
 
+}
+else if(charCode == 8){
+    localStorage.display=localStorage.getItem("display").slice(0,-1);
+    localStorage.result = localStorage.getItem("result").slice(0,-1);
+    setVal();
+}
+else if(charCode == 42 || charCode== 43 || charCode == 45 || charCode ==47){
+    localStorage.result = String(eval(localStorage.getItem("result")));
+    document.getElementById("value").innerHTML="";
+    localStorage.num = localStorage.getItem(result);
+    localStorage.result = localStorage.getItem("result")+String.fromCharCode(charCode);
+    localStorage.display = localStorage.getItem("display")+String.fromCharCode(charCode);
+   // console.log(num);
+    //console.log(result);
+    setVal();
+    
+}
+else if( charCode>= 48 && charCode<=57){
+    localStorage.result = localStorage.getItem("result")+String.fromCharCode(charCode);
+    localStorage.display = localStorage.getItem("display")+String.fromCharCode(charCode);
+    setVal();
+}
+else {
+    alert("input not valid");
+    document.getElementById("value").innerHTML= "";
+    setVal();
+}
+
+console.log(localStorage.getItem("display")+"d");
+console.log(localStorage.getItem("result")+"r");
+console.log(localStorage.getItem("num")+"n");
+}
+
+
+function setVal(){
+    document.getElementById("span").innerHTML = localStorage.getItem("display");
+document.getElementById("value").innerHTML= localStorage.getItem("num");
 }
