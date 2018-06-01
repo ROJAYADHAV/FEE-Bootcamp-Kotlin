ab76ae93c5f76161a825c8fcc27b19242e929907
Chandana Jampala <cjampala@virtusa.com>
2018-06-01 15:35:27 +0530

    car-game added

--- js/keyboardevents.js
+++ js/keyboardevents.js
@@ -1,12 +1,24 @@
+
+var result="";
+var display="";
+var num="";
+ localStorage.setItem("result",result);
+ localStorage.setItem("display",display);
+ localStorage.setItem("num",num);
+ 
+ 
 function getNumber(evt){
    
       
- var charCode = (evt.which) ? evt.which : event.keyCode  //reflects the typed content
-   console.log(charCode);
-   if (charCode > 31 && (charCode < 42 || charCode > 57)  && charCode==44 && charCode !=61)
-      return false;
-
-   return true;
+ // var charCode = (evt.which) ? evt.which : event.keyCode  //reflects the typed 
+// if(!(charCode>=48 && charCode<=57 ||charCode==42||charCode==43||charCode==45||charCode==46||charCode==47))
+  //  return false;
+ /* // console.log(charCode);
+   //if (charCode > 31 && (charCode < 42 || charCode > 57)&&charCode)
+    if((charCode>41 && charcode <58)&& charcode!=44)
+      return true;
+else
+   return false;
 /*if((charCode>41  &&  charCode<58 && charCode!=44 )|| charCode ==61 ||charCode ==13){
        return true;
    }
@@ -16,6 +28,20 @@ function getNumber(evt){
 */
 
 
+/*if(charCode==13){
+    console.log(eval(result));
+}
+else {
+var res = String(String.fromCharCode(charCode));
+  result = result + res;
+  console.log(result);
+  document.getElementById("span").innerHTML=result;
+  
+}*/
+
 
 
 }
+
+
+
